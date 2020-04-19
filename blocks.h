1f643357a49973f9728e000984760d5fca75d964
hisakiyo <hisakiyo.games@gmail.com>
2020-04-19 22:32:13 +0200

    don

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "modules/music.sh",                                          	1,              8},
+	{"", "modules/torrent.sh",						20,		0},	
 	{"", "modules/pacpackages.sh",						30,		0},
 	
-	{"", "modules/volume.sh",						5,		0},
+	{"", "modules/volume.sh",						0,		8},
 
 	{"", "modules/clock.sh",						60,		0},
 };
